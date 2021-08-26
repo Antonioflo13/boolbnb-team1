a73ce942791f226f0d59142a0a32b8dba88749aa
Antonioflo13 <floreantonio@hotmail.it>
2021-08-26 11:22:12 +0200

    add: fontawesone, fontsource, auth

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,7 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use Illuminate\Support\Facades\Auth;
 
 /*
 |--------------------------------------------------------------------------
@@ -12,11 +13,16 @@ use Illuminate\Support\Facades\Route;
 | contains the "web" middleware group. Now create something great!
 |
 */
-
-Route::get('/', function () {
-    return view('welcome');
-});
-
 Auth::routes();
 
-Route::get('/home', 'HomeController@index')->name('home');
+Route::middleware('auth')
+    ->namespace('Admin')
+    ->name('admin.')
+    ->prefix('admin')
+    ->group( function() {
+
+        Route::get('/', 'HomeController@index')->name('home');
+
+});
+
+Route::get('{any?}', 'HomeController@index')->where('any', '.*')->name('home');
