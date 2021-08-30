9abd74569b09d0846a9cfcd01c0b7e2cddfffb4f
Antonioflo13 <floreantonio@hotmail.it>
2021-08-30 12:54:03 +0200

    add: view migration, seedere, model

---
+++ database/migrations/2021_08_30_104538_create_views_table.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateViewsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('views', function (Blueprint $table) {
+            $table->id();
+            $table->unsignedBigInteger('appartment_id')->nullable();
+            $table->foreign('appartment_id')->references('id')->on('appartments')->onDelete('SET NULL');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('views');
+    }
+}
