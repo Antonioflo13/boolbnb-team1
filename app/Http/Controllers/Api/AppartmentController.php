05884ebbb8018445a66a6d671bb5917151fb644d
Antonioflo13 <floreantonio@hotmail.it>
2021-08-31 23:27:39 +0200

    add: Api

---
+++ app/Http/Controllers/Api/AppartmentController.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace App\Http\Controllers\Api;
+
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+
+use App\Appartment;
+
+class AppartmentController extends Controller
+{
+    public function index() {
+        $appartments = Appartment::all();
+        return response()->json($appartments);
+    }
+
+    public function show($slug) {
+        $appartment = Appartment::where('slug',$slug)->first();
+
+        return response()->json($appartment);
+    }
+}
