6f75efcaa2f29e37972f7ae88c8920667ca047ff
Antonioflo13 <floreantonio@hotmail.it>
2021-08-31 10:09:42 +0200

    scafolding scss

--- public/js/app.js
+++ public/js/app.js
@@ -49791,8 +49791,8 @@ window.axios.defaults.headers.common['X-Requested-With'] = 'XMLHttpRequest';
 /*! no static exports found */
 /***/ (function(module, exports, __webpack_require__) {
 
-__webpack_require__(/*! /Users/mariadulesova/Desktop/boolean34/boolbnb-team1/resources/js/app.js */"./resources/js/app.js");
-module.exports = __webpack_require__(/*! /Users/mariadulesova/Desktop/boolean34/boolbnb-team1/resources/sass/app.scss */"./resources/sass/app.scss");
+__webpack_require__(/*! /Users/antonioflore/Downloads/progetti/Boolean/boolbnb-team1/resources/js/app.js */"./resources/js/app.js");
+module.exports = __webpack_require__(/*! /Users/antonioflore/Downloads/progetti/Boolean/boolbnb-team1/resources/sass/app.scss */"./resources/sass/app.scss");
 
 
 /***/ })
