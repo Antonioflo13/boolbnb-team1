de9ed2188ed9a0a064d859a520ad303f618520e2
mariaDulesova <maria.dulesova@gmail.com>
2021-09-01 15:43:22 +0200

    update

--- public/js/app.js
+++ public/js/app.js
@@ -49791,8 +49791,8 @@ window.axios.defaults.headers.common['X-Requested-With'] = 'XMLHttpRequest';
 /*! no static exports found */
 /***/ (function(module, exports, __webpack_require__) {
 
-__webpack_require__(/*! C:\Users\Utente\Desktop\boolean\boolbnb-team1\resources\js\app.js */"./resources/js/app.js");
-module.exports = __webpack_require__(/*! C:\Users\Utente\Desktop\boolean\boolbnb-team1\resources\sass\app.scss */"./resources/sass/app.scss");
+__webpack_require__(/*! /Users/mariadulesova/Desktop/boolean34/boolbnb-team1/resources/js/app.js */"./resources/js/app.js");
+module.exports = __webpack_require__(/*! /Users/mariadulesova/Desktop/boolean34/boolbnb-team1/resources/sass/app.scss */"./resources/sass/app.scss");
 
 
 /***/ })
--- public/js/front.js
+++ public/js/front.js
@@ -55555,7 +55555,7 @@ var router = new vue_router__WEBPACK_IMPORTED_MODULE_1__["default"]({
 /*! no static exports found */
 /***/ (function(module, exports, __webpack_require__) {
 
-module.exports = __webpack_require__(/*! C:\Users\Utente\Desktop\boolean\boolbnb-team1\resources\js\front.js */"./resources/js/front.js");
+module.exports = __webpack_require__(/*! /Users/mariadulesova/Desktop/boolean34/boolbnb-team1/resources/js/front.js */"./resources/js/front.js");
 
 
 /***/ })
