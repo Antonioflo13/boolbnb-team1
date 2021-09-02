e5e344252890d336f3358bce922a2fd49459a273
AndreaCasentini93 <casentini.andrea1993@gmail.com>
2021-09-02 09:39:21 +0200

    refactoring

--- resources/views/admin/promotions/payment.blade.php
+++ resources/views/admin/promotions/payment.blade.php
@@ -1,127 +1,52 @@
-<!DOCTYPE html>
-<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
-    <head>
-        <meta charset="utf-8">
-        <meta name="viewport" content="width=device-width, initial-scale=1">
+@extends('layouts.app')
 
-        <title>Laravel</title>
-
-        <!-- Fonts -->
-        <link href="https://fonts.googleapis.com/css?family=Nunito:200,600" rel="stylesheet">
-
-        <!-- Styles -->
-        <style>
-            html, body {
-                background-color: #fff;
-                color: #636b6f;
-                font-family: 'Nunito', sans-serif;
-                font-weight: 200;
-                height: 100vh;
-                margin: 0;
-            }
-
-            .full-height {
-                height: 100vh;
-            }
-
-            .flex-center {
-                align-items: center;
-                display: flex;
-                justify-content: center;
-            }
-
-            .position-ref {
-                position: relative;
-            }
-
-            .top-right {
-                position: absolute;
-                right: 10px;
-                top: 18px;
-            }
-
-            .content {
-                text-align: center;
-            }
-
-            .title {
-                font-size: 84px;
-            }
-
-            .links > a {
-                color: #636b6f;
-                padding: 0 25px;
-                font-size: 13px;
-                font-weight: 600;
-                letter-spacing: .1rem;
-                text-decoration: none;
-                text-transform: uppercase;
-            }
-
-            .m-b-md {
-                margin-bottom: 30px;
-            }
-        </style>
-    </head>
-    <body>
-        @if (session('success_message'))
-            <div class="alert alert-success">
-                {{ session('success_message') }}
-            </div>
-        @endif
-
-        @if (count($errors) > 0)
-            <div class="alert alert-danger">
-                <ul>
-                    @foreach ($errors->all() as $error)
-                        <li>
-                            {{ $error }}
-                        </li>
-                    @endforeach
-                </ul>
-            </div>
-        @endif
-
-        <div class="flex-center position-ref full-height">
-            @if (Route::has('login'))
-                <div class="top-right links">
-                    @auth
-                        <a href="{{ url('/home') }}">Home</a>
-                    @else
-                        <a href="{{ route('login') }}">Login</a>
-
-                        @if (Route::has('register'))
-                            <a href="{{ route('register') }}">Register</a>
-                        @endif
-                    @endauth
+@section('content')
+    <section>
+        <div class="container">
+            @if (session('success_message'))
+                <div class="alert alert-success">
+                    {{ session('success_message') }}
                 </div>
             @endif
 
-            <div class="content">
-                <form method="post" id="payment-form" action="{{ url('/checkout') }}">
-                    @csrf
+            @if (count($errors) > 0)
+                <div class="alert alert-danger">
+                    <ul>
+                        @foreach ($errors->all() as $error)
+                            <li>
+                                {{ $error }}
+                            </li>
+                        @endforeach
+                    </ul>
+                </div>
+            @endif
 
-                    <section>
-                        <label for="amount">
-                            <span class="input-label">Amount</span>
-                            <div class="input-wrapper amount-wrapper">
-                                <input id="amount" name="amount" type="tel" min="1" placeholder="Amount" value="{{ $promotion->price }}">
+            <div class="d-flex justify-content-center align-items-center">
+                <div class="content d-flex flex-column align-items-center justify-content-center">
+                    <form method="post" id="payment-form" action="{{ url('/checkout') }}">
+                        @csrf
+
+                        <section>
+                            <label for="amount">
+                                <span class="input-label">Amount</span>
+                                <div class="input-wrapper amount-wrapper">
+                                    <input id="amount" name="amount" type="tel" min="1" placeholder="Amount" value="{{ $promotion->price }}">
+                                </div>
+                            </label>
+
+                            <div class="bt-drop-in-wrapper">
+                                <div id="bt-dropin"></div>
                             </div>
-                        </label>
-    
-                        <div class="bt-drop-in-wrapper">
-                            <div id="bt-dropin"></div>
-                        </div>
-                    </section>
-    
-                    <input id="nonce" name="payment_method_nonce" type="hidden" />
-                    <button class="button" type="submit"><span>Test Transaction</span></button>
-                </form>
-            </div>
-        </div>
+                        </section>
 
-        <script src="https://js.braintreegateway.com/web/dropin/1.31.2/js/dropin.min.js"></script>
-        <script>
+                        <input id="nonce" name="payment_method_nonce" type="hidden" />
+                        <button class="button" type="submit"><span>Test Transaction</span></button>
+                    </form>
+                </div>
+            </div>
+
+            <script src="https://js.braintreegateway.com/web/dropin/1.31.2/js/dropin.min.js"></script>
+            <script>
             var form = document.querySelector('#payment-form');
             var client_token = "{{ $token }}";
 
@@ -132,25 +57,26 @@
                 flow: 'vault'
             }
             }, function (createErr, instance) {
-            if (createErr) {
-                console.log('Create Error', createErr);
-                return;
-            }
-            form.addEventListener('submit', function (event) {
-                event.preventDefault();
-
-                instance.requestPaymentMethod(function (err, payload) {
-                if (err) {
-                    console.log('Request Payment Method Error', err);
+                if (createErr) {
+                    console.log('Create Error', createErr);
                     return;
                 }
+                form.addEventListener('submit', function (event) {
+                    event.preventDefault();
 
-                // Add the nonce to the form and submit
-                document.querySelector('#nonce').value = payload.nonce;
-                form.submit();
+                    instance.requestPaymentMethod(function (err, payload) {
+                        if (err) {
+                            console.log('Request Payment Method Error', err);
+                            return;
+                        }
+
+                    // Add the nonce to the form and submit
+                    document.querySelector('#nonce').value = payload.nonce;
+                    form.submit();
+                    });
                 });
             });
-            });
-        </script>
-    </body>
-</html>
+            </script>
+        </div>
+    </section>
+@endsection
