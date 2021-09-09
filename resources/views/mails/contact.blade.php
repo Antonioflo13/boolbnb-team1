ecc39c02b6cbf36f4304852c3ab1079367f9e988
Antonioflo13 <floreantonio@hotmail.it>
2021-09-09 23:49:44 +0200

    e-mail layout complete

--- resources/views/mails/contact.blade.php
+++ resources/views/mails/contact.blade.php
@@ -3,6 +3,10 @@
 <head>
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Ubuntu&display=swap" rel="stylesheet">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.4/css/all.min.css" integrity="sha512-1ycn6IcaQQ40/MKBW2W4Rhis/DbILU74C1vSrLJxCq57o941Ym01SwNsOMqvEBFlcgUa6xLiPY/NS5R+E6ztJQ==" crossorigin="anonymous" referrerpolicy="no-referrer" />
 
     <!-- CSRF Token -->
     <meta name="csrf-token" content="{{ csrf_token() }}">
@@ -14,6 +18,72 @@
 
     <!-- Styles -->
     <style>
+    /* common */
+
+    * {
+        box-sizing: border-box;
+        padding: 0;
+        margin: 0;
+    }
+
+    /* body */
+    body {
+        font-family: 'Ubuntu', sans-serif;
+        padding: 20px;
+        background-color: #FD395C;
+    }
+
+    /* container */
+    .container {
+        padding: 10px 0;
+        border-radius: 10px;
+        background-color: white;
+    }
+
+    /* header */
+    header {
+        display: flex;
+        flex-direction: column;
+        justify-content: center;
+        align-items: center;
+    }
+
+    header h4 {
+        color: #FD395C;
+    }
+
+    /* nav */
+    header nav {
+        display: flex;
+        justify-content: space-between;
+        padding: 20px;
+    }
+
+    nav ul li {
+        list-style: none;
+    }
+    nav ul li a {
+        padding: 10px;
+    }
+    nav ul li a:hover {
+        color: #FD395C;
+    }
+    .logo{
+        width: 40px;
+    }
+    .ms-text{
+        font-size: 20px;
+        color: #FD395C;  
+    }
+
+    /* main */
+    main {
+        background-color: #F8FAFC;
+    }
+
+    section {
+        padding: 20px;
+    }
         footer{
         background-color: #FFFFFF;
         border-top: 1px solid rgb(255, 255, 255);
@@ -50,109 +120,56 @@
     </style>
 </head>
 <body>
-    <div>
-        <nav class="navbar sticky-top navbar-expand-md navbar-light bg-white shadow-sm">
-            <div class="container">
-                {{-- <a class="navbar-brand" href="{{ url('/') }}">
-                    {{ config('app.name', 'Laravel') }}
-                </a> --}}
-                <a class="navbar-brand d-flex align-items-center" href="{{ url('/') }}">
-                    <img class='ms-logo mr-2' src="/images/logo.png" alt="Logo"> 
-                    <strong class="ms-text ml-3"> boolbnb </strong>
-                </a>
-                <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="{{ __('Toggle navigation') }}">
-                    <span class="navbar-toggler-icon"></span>
-                </button>
 
-                <div class="collapse navbar-collapse" id="navbarSupportedContent">
-                    <!-- Left Side Of Navbar -->
-                    <ul class="navbar-nav mr-auto">
+    {{-- container --}}
+    <div class="container">
+        <header>
+            <a href="{{ route('home') }}"><img class="logo" src="https://loghi-famosi.com/wp-content/uploads/2020/07/Logo-della-Airbnb.png" alt="bnblogo"></a>
+            <h4>BoolBnB</h4>
+            <nav>
+                <ul>
+                    <li>
+                        <a href="{{ route('home') }}">Search your Appartment</a>
+                        <a href="{{ route('home') }}">Become a Host</a>
+                    </li>
+                </ul>
+            </nav>
+        </header>
+        {{-- /header --}}
 
-                    </ul>
-                    
-                    <!-- Center Of Navbar -->
-                    <ul class="navbar-nav">
-                        @guest
-                        @else 
-                        <li class="nav-item">
-                            <a class="nav-link" href="{{ route('admin.inbox') }}">Inbox</a>
-                        </li>
-                        <li>
-                            <a class="nav-link" href="#">Insights</a>
-                        </li>
-                        <li class="nav-item dropdown">
-                            <a id="navbarDropdown" class="nav-link dropdown-toggle" href="#" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false" v-pre>
-                               Menu
-                            </a>
-
-                            <div class="dropdown-menu dropdown-menu-right" aria-labelledby="navbarDropdown">
-                                <a class="dropdown-item" href="{{ route('admin.appartments.index') }}">Appartments List</a>
-                                <a class="dropdown-item" href="{{ route('admin.appartments.create') }}">Add Appartment</a>
-                            </div>
-                        </li>
-                        @endguest
-                    </ul>
-         
-
-                    <!-- Right Side Of Navbar -->
-                    <ul class="navbar-nav ml-auto">
-                        <!-- Authentication Links -->
-                        @guest
-                            <li class="nav-item">
-                                <a class="nav-link" href="{{ route('login') }}">{{ __('Login') }}</a>
-                            </li>
-                            @if (Route::has('register'))
-                                <li class="nav-item">
-                                    <a class="nav-link" href="{{ route('register') }}">{{ __('Register') }}</a>
-                                </li>
-                            @endif
-                        @else
-                            <li class="nav-item dropdown">
-                                <a id="navbarDropdown" class="nav-link dropdown-toggle" href="#" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false" v-pre>
-                                    {{ Auth::user()->name }}
-                                </a>
-                                <div class="dropdown-menu dropdown-menu-right" aria-labelledby="navbarDropdown">
-                                    <a class="dropdown-item" href="{{ route('logout') }}"
-                                       onclick="event.preventDefault();
-                                                     document.getElementById('logout-form').submit();">
-                                        {{ __('Logout') }}
-                                    </a>
-                                    <form id="logout-form" action="{{ route('logout') }}" method="POST" class="d-none">
-                                        @csrf
-                                    </form>
-                                </div>
-                            </li>
-                        @endguest
-                    </ul>
-                </div>
-            </div>
-        </nav>
-
-        <main class="py-4">
-
+        {{-- main --}}
+        <main>
+            <section>
+                <h4>{{ $email->appartment->title }}</h4>
+                <h5>{{ $email->name }}</h5>
+                <h5>{{ $email->email }}</h5>
+                <p>{{ $email->message }}</p>
+            </section>
         </main>
-        <footer>
-            <div class="container ms-footer py-3">
-                <div class="row">
-                    <div class="col">
-                        <ul class="d-flex flex-wrap ms-footer-links align-items-center my-2">
-                            <li class="ms-list-group mr-3"> &copy; 2021 Boolbnb, Inc. </li> 
-                            <li class="ms-list-group mr-3"><a href="#">Privacy</a> </li>
-                            <li class="ms-list-group mr-3"><a href="#">Terms</a></li>
-                            <li class="ms-list-group mr-3"><a href="#">Sitemap</a></li>
-                        </ul>
-                    </div>
-                    <div class="col ms-social">
-                        <ul class="d-flex flex-row-reverse align-items-center my-2">
-                            <li class="ms-list-group mr-3"><a href="#"><i class="fab fa-2x fa-instagram"></i></a></li>
-                            <li class="ms-list-group mr-3"><a href="#"><i class="fab  fa-2x fa-twitter"></i></a> </li>
-                            <li class="ms-list-group mr-3"> <a href="#"><i class="fab fa-2x fa-facebook-f"></i></a></li>   
-                        </ul>
+        {{-- /main --}}
     
-                    </div>
-                </div> 
-            </div>
-        </footer>
+            <footer>
+                <div class="container ms-footer py-3">
+                    <div class="row">
+                        <div class="col">
+                            <ul class="d-flex flex-wrap ms-footer-links align-items-center my-2">
+                                <li class="ms-list-group mr-3"> &copy; 2021 Boolbnb, Inc. </li> 
+                                <li class="ms-list-group mr-3"><a href="#">Privacy</a> </li>
+                                <li class="ms-list-group mr-3"><a href="#">Terms</a></li>
+                                <li class="ms-list-group mr-3"><a href="#">Sitemap</a></li>
+                            </ul>
+                        </div>
+                        <div class="col ms-social">
+                            <ul class="d-flex flex-row-reverse align-items-center my-2">
+                                <li class="ms-list-group mr-3"><a href="#"><i class="fab fa-2x fa-instagram"></i></a></li>
+                                <li class="ms-list-group mr-3"><a href="#"><i class="fab  fa-2x fa-twitter"></i></a> </li>
+                                <li class="ms-list-group mr-3"> <a href="#"><i class="fab fa-2x fa-facebook-f"></i></a></li>   
+                            </ul>
+        
+                        </div>
+                    </div> 
+                </div>
+            </footer>
     </div>
 </body>
 </html>
