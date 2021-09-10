b7ec9b33d19259b322b1ddeec3f39751771290b9
Antonioflo13 <floreantonio@hotmail.it>
2021-09-10 18:28:54 +0200

    merge 6

--- resources/views/admin/appartments/show.blade.php
+++ resources/views/admin/appartments/show.blade.php
@@ -26,6 +26,7 @@
                 {{ session('success_message') }}
             </div>
         @endif
+
         <div>
             <div class="d-flex align-items-center justify-content-between">
                 <h1>{{ $appartment->title }}</h1>
@@ -45,10 +46,13 @@
                             <div id="demo" class="mr-3 text-success"></div>
                         </strong>
                     @endif
-                    <a href="{{ route('admin.promotions', $appartment->id) }}" class="mr-5"><i class="fas fa-sort-amount-up-alt mr-2"></i> Upgrade</a>
-                    <a href="http://127.0.0.1:8000/singlelocation/{{ $appartment->slug }}" class="btn">Preview listing</a>
-                    <button class="btn btn_delete" onclick="popup()">Delete</button>
-                    <a href="{{ route('admin.appartments.edit', $appartment->id) }}">Edit<i class="fas fa-chevron-right ml-2"></i></a>
+                    <a class="btn_listing" href="{{ route('admin.promotions', $appartment->id) }}" class="mr-5"><i class="fas fa-sort-amount-up-alt mr-2"></i> Upgrade</a>
+                    <a class="btn_listing" href="http://127.0.0.1:8000/singlelocation/{{ $appartment->slug }}" class="btn">Preview listing</a>
+                    
+                        <button onclick="popup()" class="btn btn_delete">Delete</button>
+
+                    
+                    <a class="btn_listing" href="{{ route('admin.appartments.edit', $appartment->id) }}">Edit<i class="fas fa-chevron-right ml-2"></i></a>
                 </div>
             </div>
             {{-- edit collection --}}
@@ -130,58 +134,57 @@
 </section>
 
 <script>
-        //popup
-        function popup() {
-        document.getElementsByClassName("popup")[0].className = "PopupPanel";
-        };
+    //popup
+    function popup() {
+    document.getElementsByClassName("popup")[0].className = "PopupPanel";
+    };
 
-        function popdown() {
-            document.getElementsByClassName("PopupPanel")[0].className = "popup";
+    function popdown() {
+        document.getElementsByClassName("PopupPanel")[0].className = "popup";
+    }
+
+
+    // Assign a php variable to js
+    var endPromotion = "<?php
+        if (count($appartment->promotions) > 0) {
+            echo $appartment->promotions[0]->pivot->end_promotion; 
+        } else {
+            echo '';
         }
+    ?>";
 
+    if (endPromotion.length > 0) {
 
-        // Assign a php variable to js
-        var endPromotion = "<?php
-            if (count($appartment->promotions) > 0) {
-                echo $appartment->promotions[0]->pivot->end_promotion; 
-            } else {
-                echo '';
-            }
-        ?>";
+        // Set the date we're counting down to
+        var countDownDate = new Date(endPromotion).getTime();
 
-        if (endPromotion.length > 0) {
+        // Update the count down every 1 second
+        var x = setInterval(function() {
 
-            // Set the date we're counting down to
-            var countDownDate = new Date(endPromotion).getTime();
-    
-            // Update the count down every 1 second
-            var x = setInterval(function() {
-    
-            // Get today's date and time
-            var now = new Date().getTime();
-    
-            // Find the distance between now and the count down date
-            var distance = countDownDate - now;
-    
-            // Time calculations for days, hours, minutes and seconds
-            var days = Math.floor(distance / (1000 * 60 * 60 * 24));
-            var hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
-            var minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
-            var seconds = Math.floor((distance % (1000 * 60)) / 1000);
-    
-            // Display the result in the element with id="demo"
-            document.getElementById("demo").innerHTML = days + "d " + hours + "h "
-            + minutes + "m " + seconds + "s ";
-    
-            // If the count down is finished, write some text
-            if (distance < 0) {
-                clearInterval(x);
-                document.getElementById("demo").innerHTML = "EXPIRED";
-            }
-            }, 1000);
+        // Get today's date and time
+        var now = new Date().getTime();
+
+        // Find the distance between now and the count down date
+        var distance = countDownDate - now;
+
+        // Time calculations for days, hours, minutes and seconds
+        var days = Math.floor(distance / (1000 * 60 * 60 * 24));
+        var hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+        var minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
+        var seconds = Math.floor((distance % (1000 * 60)) / 1000);
+
+        // Display the result in the element with id="demo"
+        document.getElementById("demo").innerHTML = days + "d " + hours + "h "
+        + minutes + "m " + seconds + "s ";
+
+        // If the count down is finished, write some text
+        if (distance < 0) {
+            clearInterval(x);
+            document.getElementById("demo").innerHTML = "EXPIRED";
         }
         }, 1000);
     }
 
 </script>
+
 @endsection
