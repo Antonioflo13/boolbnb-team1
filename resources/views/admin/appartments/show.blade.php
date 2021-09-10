1a937002cee81f4866603836b332fce158e7950d
francs2812 <emiliofrangella2812@gmail.com>
2021-09-10 17:30:42 +0200

    Back Show

--- resources/views/admin/appartments/show.blade.php
+++ resources/views/admin/appartments/show.blade.php
@@ -29,8 +29,8 @@
                             <div id="demo" class="mr-3 text-success"></div>
                         </strong>
                     @endif
-                    <a href="{{ route('admin.promotions', $appartment->id) }}" class="mr-5"><i class="fas fa-sort-amount-up-alt mr-2"></i> Upgrade</a>
-                    <a href="http://127.0.0.1:8000/singlelocation/{{ $appartment->slug }}" class="btn">Preview listing</a>
+                    <a class="btn_listing" href="{{ route('admin.promotions', $appartment->id) }}" class="mr-5"><i class="fas fa-sort-amount-up-alt mr-2"></i> Upgrade</a>
+                    <a class="btn_listing" href="http://127.0.0.1:8000/singlelocation/{{ $appartment->slug }}" class="btn">Preview listing</a>
                     
                     <form action="{{ route('admin.appartments.destroy', $appartment->id) }}" method="POST">
                         @csrf
@@ -38,7 +38,7 @@
                         <button class="btn btn_delete">Delete</button>
                     </form>
                     
-                    <a href="{{ route('admin.appartments.edit', $appartment->id) }}">Edit<i class="fas fa-chevron-right ml-2"></i></a>
+                    <a class="btn_listing" href="{{ route('admin.appartments.edit', $appartment->id) }}">Edit<i class="fas fa-chevron-right ml-2"></i></a>
                 </div>
             </div>
             {{-- edit collection --}}
@@ -47,9 +47,9 @@
                     {{-- edit menu --}}
                     <div class="col-sm-8 col-lg-3 mb-3">
                       <div class="list-group" id="list-tab" role="tablist">
-                        <a href="#" class="list-group-item disabled" aria-disabled="true">Listing Details</a>
-                        <a class="list-group-item list-group-item-action active" id="list-photos-list" data-toggle="list" href="#list-photos" role="tab" aria-controls="photos">Photos</a>
-                        <a class="list-group-item list-group-item-action" id="list-listing-basics-list" data-toggle="list" href="#list-listing-basics" role="tab" aria-controls="listing-basics">Listing Basics</a>
+                        <a href="#" class="list-group-item disabled listing" aria-disabled="true">Listing Details</a>
+                        <a class="list-group-item list-group-item-action active btn_listing" id="list-photos-list" data-toggle="list" href="#list-photos" role="tab" aria-controls="photos">Photos</a>
+                        <a class="list-group-item list-group-item-action btn_listing" id="list-listing-basics-list" data-toggle="list" href="#list-listing-basics" role="tab" aria-controls="listing-basics">Listing Basics</a>
                       </div>
                     </div>
                     {{-- /edit menu --}}
@@ -86,16 +86,19 @@
                                 </div>
                                 <div class="d-flex align-items-center justify-content-between my-3">
                                     <h5>Number of guest</h5>
-                                    <p>{{ $appartment->beds_number }}</p>
+
+                                    @for ($i = 0; $i < $appartment->beds_number; $i++)
+                                        <i class="fas fa-male"></i>
+                                    @endfor
                                 </div>
                                 <div class="my-3">
                                     <h5>Listing Status</h5>
                                     @if (!$appartment->visible) 
                                         {{-- pallino rosso --}}
-                                        <span class="mr-5 indicator"><i class="fas fa-circle mr-2"></i> Not Available</span>
+                                        <span class="mr-5 indicator"><i style="color: red" class="fas fa-circle mr-2"></i> Not Available</span>
                                     @else
                                         {{-- pallino verde --}}
-                                        <span class="mr-5 indicator"><i class="fas fa-circle mr-2"></i> Available - Guest can find your listing in search result and request or book available dates.</span>
+                                        <span class="mr-5 indicator"><i style="color: green" class="fas fa-circle mr-2"></i> Available - Guest can find your listing in search result and request or book available dates.</span>
                                     @endif
                                 </div>
                                 <div class="my-3">
@@ -117,46 +120,46 @@
 </section>
 
 <script>
-        // Assign a php variable to js
-        var endPromotion = "<?php
-            if (count($appartment->promotions) > 0) {
-                echo $appartment->promotions[0]->pivot->end_promotion; 
-            } else {
-                echo '';
-            }
-        ?>";
-
-        if (endPromotion.length > 0) {
-
-            // Set the date we're counting down to
-            var countDownDate = new Date(endPromotion).getTime();
-    
-            // Update the count down every 1 second
-            var x = setInterval(function() {
-    
-            // Get today's date and time
-            var now = new Date().getTime();
-    
-            // Find the distance between now and the count down date
-            var distance = countDownDate - now;
-    
-            // Time calculations for days, hours, minutes and seconds
-            var days = Math.floor(distance / (1000 * 60 * 60 * 24));
-            var hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
-            var minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
-            var seconds = Math.floor((distance % (1000 * 60)) / 1000);
-    
-            // Display the result in the element with id="demo"
-            document.getElementById("demo").innerHTML = days + "d " + hours + "h "
-            + minutes + "m " + seconds + "s ";
-    
-            // If the count down is finished, write some text
-            if (distance < 0) {
-                clearInterval(x);
-                document.getElementById("demo").innerHTML = "EXPIRED";
-            }
-            }, 1000);
+    // Assign a php variable to js
+    var endPromotion = "<?php
+        if (count($appartment->promotions) > 0) {
+            echo $appartment->promotions[0]->pivot->end_promotion; 
+        } else {
+            echo '';
         }
+    ?>";
+
+    if (endPromotion.length > 0) {
+
+        // Set the date we're counting down to
+        var countDownDate = new Date(endPromotion).getTime();
+
+        // Update the count down every 1 second
+        var x = setInterval(function() {
+
+        // Get today's date and time
+        var now = new Date().getTime();
+
+        // Find the distance between now and the count down date
+        var distance = countDownDate - now;
+
+        // Time calculations for days, hours, minutes and seconds
+        var days = Math.floor(distance / (1000 * 60 * 60 * 24));
+        var hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+        var minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
+        var seconds = Math.floor((distance % (1000 * 60)) / 1000);
+
+        // Display the result in the element with id="demo"
+        document.getElementById("demo").innerHTML = days + "d " + hours + "h "
+        + minutes + "m " + seconds + "s ";
+
+        // If the count down is finished, write some text
+        if (distance < 0) {
+            clearInterval(x);
+            document.getElementById("demo").innerHTML = "EXPIRED";
+        }
+        }, 1000);
+    }
 
 </script>
 @endsection
