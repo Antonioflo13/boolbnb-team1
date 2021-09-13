d9046d62638711b136358ddd86e606868d24f580
AndreaCasentini93 <casentini.andrea1993@gmail.com>
2021-09-13 14:00:16 +0200

    show refactoring

--- resources/views/admin/appartments/show.blade.php
+++ resources/views/admin/appartments/show.blade.php
@@ -29,7 +29,7 @@
 
         <div>
             <div class="d-flex align-items-center justify-content-between">
-                <h1>{{ $appartment->title }}</h1>
+                <h1 class="mr-2">{{ $appartment->title }}</h1>
                 <div class="d-flex align-items-center">
                     @if (!$appartment->visible) 
                         {{-- pallino rosso --}}
@@ -114,7 +114,7 @@
                                         <span class="mr-5 indicator"><i style="color: red" class="fas fa-circle mr-2"></i> Not Available</span>
                                     @else
                                         {{-- pallino verde --}}
-                                        <span class="mr-5 indicator"><i style="color: green" class="fas fa-circle mr-2"></i> Available - Guest can find your listing in search result and request or book available dates.</span>
+                                        <span class="indicator"><i style="color: green" class="fas fa-circle mr-2"></i> Available - Guest can find your listing in search result and request or book available dates.</span>
                                     @endif
                                 </div>
                                 <div class="my-3">
@@ -176,7 +176,7 @@
         var seconds = Math.floor((distance % (1000 * 60)) / 1000);
 
         // Display the result in the element with id="demo"
-        document.getElementById("demo").innerHTML = days + "d " + hours + "h " + minutes + "m";
+        document.getElementById("demo").innerHTML = days + "d " + hours + "h " + minutes + "m " + seconds + "s";
 
         // If the count down is finished, write some text
         if (distance < 0) {
