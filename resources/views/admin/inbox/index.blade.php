327517aa7d3629cc025f9c943264c77c04bea1e7
Antonioflo13 <floreantonio@hotmail.it>
2021-09-13 11:22:02 +0200

    refactoring inbox

--- resources/views/admin/inbox/index.blade.php
+++ resources/views/admin/inbox/index.blade.php
@@ -25,25 +25,31 @@
         {{-- /popup --}}
 
         <article  class="container">
-            @foreach ($singleAppartmentsMessagges as $singleAppartmentsMessagge)
-                    <div class="card">
-                        <h3>{{ $singleAppartmentsMessagge->appartment->title }}</h3>
-                        <h4>{{ $singleAppartmentsMessagge->name }}</h4>
-                        <div class="d-flex">
-                            <textarea name="" id="area" cols="30" rows="1" disabled>{{ $singleAppartmentsMessagge->email }}</textarea>
-                            <button onclick="copy('area')" class="ml-3"><i class="far fa-copy"></i></button>
-                        </div>
-                        <button class="ms-btn-filter" type="button" data-toggle="collapse" data-target="#collapseExample{{ $singleAppartmentsMessagge->id }}" aria-expanded="false" aria-controls="collapseExample">
-                            Show Message
-                        </button>
-                        <div class="collapse mt-3" id="collapseExample{{ $singleAppartmentsMessagge->id }}">
-                            <div>
-                                <p>{{ $singleAppartmentsMessagge->message }}</p>
-                                    <button class="btn btn_delete mb-5" onclick="popup()"><i class="fas fa-trash-alt"></i></button>
+            @if (count($singleAppartmentsMessagges) == 0)
+                <div class="d-flex justify-content-center align-items-center" style="height: calc(100vh - 229.25px)">
+                    <h2>No Messages Found</h2>
+                </div>
+            @else
+                @foreach ($singleAppartmentsMessagges as $singleAppartmentsMessagge)
+                        <div class="card">
+                            <h3>{{ $singleAppartmentsMessagge->appartment->title }}</h3>
+                            <h4>{{ $singleAppartmentsMessagge->name }}</h4>
+                            <div class="d-flex">
+                                <textarea name="" id="area" cols="30" rows="1" disabled>{{ $singleAppartmentsMessagge->email }}</textarea>
+                                <button onclick="copy('area')" class="ml-3"><i class="far fa-copy"></i></button>
+                            </div>
+                            <button class="ms-btn-filter" type="button" data-toggle="collapse" data-target="#collapseExample{{ $singleAppartmentsMessagge->id }}" aria-expanded="false" aria-controls="collapseExample">
+                                Show Message
+                            </button>
+                            <div class="collapse mt-3" id="collapseExample{{ $singleAppartmentsMessagge->id }}">
+                                <div>
+                                    <p>{{ $singleAppartmentsMessagge->message }}</p>
+                                        <button class="btn btn_delete mb-5" onclick="popup()"><i class="fas fa-trash-alt"></i></button>
+                                </div>
                             </div>
                         </div>
-                    </div>
-            @endforeach
+                @endforeach
+            @endif
 
             {{-- paginate --}}
             <div class="d-flex justify-content-center align-items-center">
